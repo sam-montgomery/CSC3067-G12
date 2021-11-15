3cbf72fb709e1b0c1b9075b29d4c398bc5377727
40229998 <smontgomery27@qub.ac.uk>
2021-11-15 12:55:59 +0000

    Loading the training dataset and labels.

--- loadTrainingDataset.m
+++ loadTrainingDataset.m
@@ -1,10 +1,13 @@
-function [images, label] = loadTrainingDataset()
+function [images, labels] = loadTrainingDataset()
     imagesFolder = 'images';
+    noOfPosFiles = 0;
 
     if ~isfolder(imagesFolder)
         errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', imagesFolder);
         uiwait(warndlg(errorMessage));
     end
+
+   % trainingImagesStore = imageDatastore(imagesFolder,'IncludeSubfolders',true,'LabelSource','foldernames');
 
     %load positive files
     posFilePattern = fullfile(imagesFolder, '\pos\*.jpg'); % Change to whatever pattern you need.
@@ -14,6 +17,9 @@ function [images, label] = loadTrainingDataset()
         fullFileName = fullfile(posFiles(i).folder, posFiles(i).name);
         %pre processing function
         greyImPos = rgb2gray(imread(fullFileName));
+        images(i,:) = reshape(greyImPos, 1, []);
+        labels(i,1) = 1;
+        noOfPosFiles = i;
     end
 
     %load negative files
@@ -23,7 +29,10 @@ function [images, label] = loadTrainingDataset()
     for i=1:length(negFiles)
         fullFileName = fullfile(negFiles(i).folder, negFiles(i).name);
         %pre processing function
-        greyImNeg = rgb2gray(imread(fullFileName));
+        greyImNeg = im2gray(imread(fullFileName));
+        index = i + noOfPosFiles;
+        images(index,:) = reshape(greyImNeg, 1, []);
+        labels(index,1) = -1;
     end
    
 end
