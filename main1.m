22909cd207eeec62d79099cce4a36658952314bd
40231256 <rmcsorley06@qub.ac.uk>
2021-12-04 00:04:57 +0000

    Evaluation Metrics for SVM HH

--- main1.m
+++ main1.m
@@ -8,14 +8,15 @@ close all;
 
 [images, labels] = loadTrainingDataset();
 %testDataset = loadTestDataset();
+iteration = 20;
 
 randOrder = randperm(size(images,1));
-for i=1:20
+for i=1:iteration
     trainingSet.images(i,:) = images(randOrder(i),:);
     trainingSet.labels(i,:) = labels(randOrder(i),:);
 end
 
-for i=1:20
+for i=1:iteration
     index = size(images,1)/2 + i;
     index = int64(index) - 1;
     testSet.images(i,:) = images(randOrder(index),:);
@@ -23,5 +24,9 @@ for i=1:20
 end
 
 %SVMModel = trainSVM(trainingSet.images, trainingSet.labels);
-modelAccuracyHH_SVM(trainingSet, testSet);
-
+[accuracy, TP, FP, TN, FN] = modelAccuracyHH_SVM(trainingSet, testSet, iteration);
+recall = TP/(TP+FN);
+precision = TP/(TP+FP);
+specificity = TN/(TN+FP);
+f_measure = (2*TN)/(2*TP+FN+FP);
+false_alarm_rate = FP/(FP+TN);
--- modelAccuracyHH_SVM.m
+++ modelAccuracyHH_SVM.m
@@ -1,4 +1,4 @@
-function accuracy = modelAccuracyHH_SVM(trainingDataset, testDataset)
+function [accuracy, TP, FP, TN, FN] = modelAccuracyHH_SVM(trainingDataset, testDataset, iteration)
     %For half and half accuracy, take half of the provided training dataset and pass
     %it in as trainingDataset, train the model based on this, and test it
     %with testDataset, which should be the other half of the original
@@ -12,6 +12,27 @@ function accuracy = modelAccuracyHH_SVM(trainingDataset, testDataset)
         classificationResult(i,:) = testSVM(im, model);
     end
     toc;
+
+    TP = 0;
+    FP = 0;
+    TN = 0;
+    FN = 0;
+
+    for i =1:iteration
+        if(testDataset.labels(i) == 1 && classificationResult(i) == 1)
+            TP = TP + 1;
+        end
+        if(testDataset.labels(i) == -1 && classificationResult(i) == 1)
+            FP = FP + 1;
+        end
+        if(testDataset.labels(i) == -1 && classificationResult(i) == -1)
+            TN = TN + 1;
+        end
+        if(testDataset.labels(i) == 1 && classificationResult(i) == -1)
+            FN = FN + 1;
+        end
+    end
+    
     comparison = (testDataset.labels==classificationResult);
     accuracy = sum(comparison)/length(comparison);
 end
