a0afeab5649379a6ae1ecb487a9e8b9fa2bd04b6
40229998 <smontgomery27@qub.ac.uk>
2021-12-06 21:40:33 +0000

    Added methods for getting training and testing sets

---
+++ HOG/loadHOGDataset.m
@@ -0,0 +1,12 @@
+function [trainingSet, testingSet] = loadHOGDataset(samplesize)
+    [images, labels] = loadTrainingDatasetHOG(samplesize);
+
+    for i=1:2:size(images,1)
+        trainingSet.images(i,:) = images(i,:);
+        trainingSet.labels(i,:) = labels(i,:);
+    end
+    for i=2:2:size(images,1)
+        testingSet.images(i,:) = images(i,:);
+        testingSet.labels(i,:) = labels(i,:);
+    end
+end
