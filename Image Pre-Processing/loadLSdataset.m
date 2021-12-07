2fd187934594c93158c759b133f19aa703ab9b5b
40237392 <michaelhawkins05@outlook.com>
2021-12-07 19:06:02 +0000

    pre-processing linear stretching

---
+++ Image Pre-Processing/loadLSdataset.m
@@ -0,0 +1,46 @@
+%loads dataset of full images ensuring half is positive and half is
+%negative
+
+function [images, labels] = loadLSdataset(samplesize)
+    imagesFolder = '..\images';
+    noOfPosFiles = 0;
+
+    if ~isfolder(imagesFolder)
+        errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', imagesFolder);
+        uiwait(warndlg(errorMessage));
+    end
+
+    %load positive files
+    posFilePattern = fullfile(imagesFolder, '\pos\*.jpg'); % Change to whatever pattern you need.
+    posFiles = dir(posFilePattern);
+    
+    randI = randi(size(posFiles, 1), 1, samplesize);
+
+    for i=1:samplesize/2
+        fullFileName = fullfile(posFiles(randI(i)).folder, posFiles(randI(i)).name);
+        %pre processing function
+        greyImPos = rgb2gray(imread(fullFileName));
+        lsImage = enhanceContrastALS(greyImPos);
+        beImage = enhanceBrightness(lsImage, 50);
+        images(i,:) = reshape(beImage, 1, []);
+        labels(i,1) = 1;
+        noOfPosFiles = i;
+    end
+
+    %load negative files
+    negFilePattern = fullfile(imagesFolder, '\neg\*.jpg'); % Change to whatever pattern you need.
+    negFiles = dir(negFilePattern);
+
+    randI = randi(size(negFiles, 1), 1, samplesize);
+    for i=1:samplesize/2
+        fullFileName = fullfile(negFiles(randI(i)).folder, negFiles(randI(i)).name);
+        %pre processing function
+        greyImNeg = im2gray(imread(fullFileName));
+        lsImage = enhanceContrastALS(greyImNeg);
+        beImage = enhanceBrightness(lsImage, 50);
+        index = i + noOfPosFiles;
+        images(index,:) = reshape(beImage, 1, []);
+        labels(index,1) = -1;
+    end
+   images = double(images);
+end
