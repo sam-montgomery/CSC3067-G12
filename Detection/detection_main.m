096c28d231ad36fbccedb8dfee28b04c50bc8d62
Sam Montgomery <40229998@eeecs.qub.ac.uk>
2021-12-07 13:09:06 +0000

    Fixing datatset methods

--- Detection/detection_main.m
+++ Detection/detection_main.m
@@ -1,11 +1,15 @@
 clear all;
 close all;
 
-[images, labels] = loadTrainingDatasetHOG(2000);
+% [images, labels] = loadTrainingDatasetHOG(1500);
+
+[trainingSet, testingSet] = loadFullImageDataset(20);
 
 testDataset = loadTestDataset();
 
 SVMModel = SVMtraining(images, labels);
+
+testHOGMetricsSVM(1500, images, labels);
 %modelNN = trainNN(trainingImages, trainingLabels);
-image = testDataset.images(1,10);
+image = testDataset.images(1,1);
 detect(image, SVMModel);
